ec6496dc45e7f9939bb0b83ffae140b9f804816e
guigang xie <xie.guigang@gcmodeller.org>
2023-01-31 10:52:11 +0800

    Create CustomFunction.vb

---
+++ src/enigma/Model/CustomFunction.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.Activations
+
+Public Class CustomFunction : Inherits IActivationFunction
+
+    Public Overrides ReadOnly Property Store As ActiveFunction
+        Get
+            Return New ActiveFunction With {
+                .name = "",
+                .Arguments = {}
+            }
+        End Get
+    End Property
+
+    ReadOnly lambda As Func(Of Double, Double)
+
+    Public Overrides Function [Function](x As Double) As Double
+        Return lambda(x)
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return Store.ToString
+    End Function
+
+    Protected Overrides Function Derivative(x As Double) As Double
+        Throw New NotImplementedException()
+    End Function
+End Class
