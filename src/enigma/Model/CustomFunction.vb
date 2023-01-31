d01df1bc6befc249b9ed582787e33a3ef0892bbd
guigang xie <xie.guigang@gcmodeller.org>
2023-01-31 11:05:56 +0800

    Update CustomFunction.vb

--- src/enigma/Model/CustomFunction.vb
+++ src/enigma/Model/CustomFunction.vb
@@ -1,27 +1,33 @@
-﻿Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.Activations
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.Activations
+Imports Microsoft.VisualBasic.Text.Xml.Models
 
 Public Class CustomFunction : Inherits IActivationFunction
 
     Public Overrides ReadOnly Property Store As ActiveFunction
         Get
             Return New ActiveFunction With {
-                .name = "",
-                .Arguments = {}
+                .name = "f",
+                .Arguments = {New NamedValue("x", "0.0")}
             }
         End Get
     End Property
 
-    ReadOnly lambda As Func(Of Double, Double)
+    ReadOnly m_lambda As Func(Of Double, Double)
+    ReadOnly m_derivative As Func(Of Double, Double)
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Overrides Function [Function](x As Double) As Double
-        Return lambda(x)
+        Return m_lambda(x)
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Overrides Function ToString() As String
         Return Store.ToString
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Protected Overrides Function Derivative(x As Double) As Double
-        Throw New NotImplementedException()
+        Return m_derivative(x)
     End Function
 End Class
