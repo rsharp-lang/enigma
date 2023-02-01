a4ad7ed8c4505503888711d3d53a7928206b5b5d
guigang xie <xie.guigang@gcmodeller.org>
2023-02-01 14:31:05 +0800

    model is still not working as expected

--- test/regression.R
+++ test/regression.R
@@ -6,17 +6,21 @@ data("mtcars");
 
 data = mtcars;
 
+for(i in ["disp", "hp", "drat", "wt", "qsec"]) {
+    data[, i] = data[, i] / max(data[, i]);
+}
+
 print(data, max.print = 6);
 
 tensor(model = ANN)
-|> feed(data, features = ["cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb"])
+|> feed(data, features = ["disp", "hp", "drat", "wt", "qsec"])
 |> hidden_layer([13, 25, 15], activate = activateFunction::qlinear)
 |> output_layer(labels = "mpg", activate = activateFunction::qlinear)
-|> learn(truncate = 25, threshold = 5)
+|> learn(truncate = -1, threshold = 3)
 # |> snapshot(file = "./model.hds")
 # ;
 # tensor(model = "./model.hds")
-|> solve(mtcars)
+|> solve(data)
 |> print()
 ;
 
