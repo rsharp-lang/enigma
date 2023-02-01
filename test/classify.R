a7542f357e88fe90290d9ca28dd8d427eb6ba4dc
guigang xie <xie.guigang@gcmodeller.org>
2023-02-01 15:19:22 +0800

    predictions still has errors

--- test/classify.R
+++ test/classify.R
@@ -18,7 +18,7 @@ print(data, max.print = 6);
 
 tensor(model = ANN)
 |> feed(data, features = ["D1","D2","D3","D4"])
-|> hidden_layer([10, 50, 5], activate = activateFunction::sigmoid(alpha = 2.0))
+|> hidden_layer([100, 500, 5], activate = activateFunction::sigmoid(alpha = 2.0))
 |> output_layer(labels = ["class.Iris-setosa","class.Iris-versicolor","class.Iris-virginica"], activate = activateFunction::sigmoid(alpha = 2.0))
 |> learn()
 # |> snapshot(file = "./model.hds")
