348f8298c46e2dc178475b0c9339dce4a8512ada
guigang xie <xie.guigang@gcmodeller.org>
2023-02-01 09:11:31 +0800

    build updates of the function documents

--- test/regression.R
+++ test/regression.R
@@ -12,7 +12,7 @@ tensor(model = ANN)
 |> feed(data, features = ["cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb"])
 |> hidden_layer([13, 25, 15], activate = activateFunction::sigmoid(alpha = 2.0))
 |> output_layer(labels = "mpg", activate = activateFunction::func(
-    forward = x -> log(x ^ 2), 
+    forward    = x -> log(x ^ 2), 
     derivative = x -> 1 / (2 * x)
 ))
 |> learn()
