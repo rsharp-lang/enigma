f391e2ed18ca281f38e25914fd775a403de0fec4
guigang xie <xie.guigang@gcmodeller.org>
2023-02-01 15:12:17 +0800

    run classify test success

--- man/enigma/activateFunction/activateFunction.md
+++ man/enigma/activateFunction/activateFunction.md
@@ -4,4 +4,5 @@ the activate function handler
 
 + [sigmoid](activateFunction/sigmoid.1) Create sigmoid activation function
 + [identical](activateFunction/identical.1) 
++ [qlinear](activateFunction/qlinear.1) 
 + [func](activateFunction/func.1) create a new custom activate function
--- test/classify.R
+++ test/classify.R
@@ -7,23 +7,23 @@ data("bezdekIris");
 
 data = bezdekIris 
 |> toFeatureSet() 
-|> dataset::encoding() 
+|> dataset::encoding(class = to_factors) 
 |> as.data.frame();
 
 print(data, max.print = 6);
 
-stop();
+# stop();
 
 tensor(model = ANN)
 |> feed(data, features = ["D1","D2","D3","D4"])
-|> hidden_layer([10, 25, 15], activate = activateFunction::sigmoid(alpha = 2.0))
-|> output_layer(labels = ["","","",""], activate = activateFunction::sigmoid(alpha = 2.0))
+|> hidden_layer([10, 50, 5], activate = activateFunction::sigmoid(alpha = 2.0))
+|> output_layer(labels = ["class.Iris-setosa","class.Iris-versicolor","class.Iris-virginica"], activate = activateFunction::sigmoid(alpha = 2.0))
 |> learn()
 # |> snapshot(file = "./model.hds")
 # ;
 
 # tensor(model = "./model.hds")
 |> solve(data)
-|> print()
+|> write.csv(file = "./bezdekIris_class.csv")
 ;
 
