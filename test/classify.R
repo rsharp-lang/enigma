89aa774994026b25f7afb3281e09a209258ade99
guigang xie <xie.guigang@gcmodeller.org>
2023-02-01 14:58:24 +0800

    go to test the classify problem

--- test/classify.R
+++ test/classify.R
@@ -1,23 +1,29 @@
 require(enigma);
 
 imports ["learning", "model", "activateFunction"] from "enigma";
+imports "dataset" from "MLkit";
 
 data("bezdekIris");
 
-data = bezdekIris;
+data = bezdekIris 
+|> toFeatureSet() 
+|> dataset::encoding() 
+|> as.data.frame();
 
 print(data, max.print = 6);
 
+stop();
+
 tensor(model = ANN)
 |> feed(data, features = ["D1","D2","D3","D4"])
-|> hidden_layer([10, 25, 15], activate = "Sigmoid(alpha = 2.0)")
-|> output_layer(activate = "Sigmoid(alpha = 2.0)")
+|> hidden_layer([10, 25, 15], activate = activateFunction::sigmoid(alpha = 2.0))
+|> output_layer(labels = ["","","",""], activate = activateFunction::sigmoid(alpha = 2.0))
 |> learn()
-|> snapshot(file = "./model.hds")
-;
+# |> snapshot(file = "./model.hds")
+# ;
 
-tensor(model = "./model.hds")
-|> solve(bezdekIris)
+# tensor(model = "./model.hds")
+|> solve(data)
 |> print()
 ;
 
