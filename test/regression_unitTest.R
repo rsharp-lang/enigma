4f6f57f389ef47d5ef567ccdf80c7fb9009c2a95
xieguigang <gg.xie@bionovogene.com>
2023-02-02 15:30:45 +0800

    Update regression_unitTest.R

--- test/regression_unitTest.R
+++ test/regression_unitTest.R
@@ -2,9 +2,9 @@ require(enigma);
 
 imports ["learning", "model", "activateFunction"] from "enigma";
 
-x = 1:1000;
-y = x ^ 1.25;
-z = y / x;
+let x = 1:1000;
+let y = x ^ 1.25;
+let z = y / x;
 
 data = data.frame(x,y,z, row.names = as.character(x));
 
