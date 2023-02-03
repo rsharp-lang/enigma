69cb428f62e7e2f914f10d32d75c69035221cefd
xieguigang <xie.guigang@gcmodeller.org>
2023-02-04 01:54:02 +0800

    do svm model training

--- src/enigma/Model/SVMModel.vb
+++ src/enigma/Model/SVMModel.vb
@@ -1,12 +1,19 @@
 ﻿Imports Microsoft.VisualBasic.MachineLearning
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Public Class SVMModel : Inherits MLModel
 
     Public Overrides Function DoCallTraining(args As list, env As Environment) As MLModel
-        Dim problem As SVM.Problem
-        Dim params As SVM.Parameter
+        Dim labels As String() = REnv.asVector(Of String)(data(Me.Labels(Scan0)))
+        Dim problem = svmDataSet.svmProblem(Features, labels, data, env)
+        Dim params As New SVM.Parameter
+
+        If problem Like GetType(Message) Then
+            Call problem.TryCast(Of Message).ThrowCLRError()
+        End If
 
         Me.Model = SVM.Train(problem, params)
 
