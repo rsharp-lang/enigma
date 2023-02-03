0dcfb473b607f87c6b0e438e46a895aa5756bf09
guigang xie <xie.guigang@gcmodeller.org>
2023-02-03 13:09:54 +0800

    Create XGBoost.vb

---
+++ src/enigma/Model/XGBoost.vb
@@ -0,0 +1,13 @@
+﻿Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Public Class XGBoost : Inherits MLModel
+
+    Public Overrides Function DoCallTraining(args As list, env As Environment) As MLModel
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function Solve(data As Object, env As Environment) As Object
+        Throw New NotImplementedException()
+    End Function
+End Class
