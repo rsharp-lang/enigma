820a0ece8e3716abfadb7d1f3c2399c4c00e0cd5
xieguigang <xie.guigang@gcmodeller.org>
2023-02-04 00:18:19 +0800

    commit nothing

---
+++ src/enigma/Model/SVMModel.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.MachineLearning
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Public Class SVMModel : Inherits MLModel
+
+    Public Overrides Function DoCallTraining(args As list, env As Environment) As MLModel
+        Dim problem As SVM.Problem
+        Dim params As SVM.Parameter
+
+        Me.Model = SVM.Train(problem, params)
+
+        Return Me
+    End Function
+
+    Public Overrides Function Solve(data As Object, env As Environment) As Object
+
+    End Function
+End Class
