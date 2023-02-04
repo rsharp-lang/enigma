0cf032d3c42287e9b564c1795d5ac547f8771717
xieguigang <xie.guigang@gcmodeller.org>
2023-02-05 02:27:26 +0800

    run error visualization plot test

--- test/testErrors.R
+++ test/testErrors.R
@@ -2,18 +2,32 @@ require(ggplot);
 
 setwd(@dir);
 
-# test errors of SVR
-data = read.csv("./svr_test.csv", row.names = 1);
-data[, "errors"] = abs(data$z.predicts. - data$z);
+plot_errors = function(data, title, file) {
+    data[, "errors"] = abs(data$z.predicts. - data$z);
 
-print(data, max.print =6);
+    print(data, max.print =6);
 
-bitmap(file = "./svr_test.png", size = [2700, 1800]) {
-    ggplot(data, aes(x = "errors"), padding = "padding:250px 400px 200px 250px;")
-	 + geom_histogram(bins = 250,  color = "steelblue")
-	 + ggtitle("Frequency of SVR Prediction Errors")
-	 + scale_x_continuous(labels = "F2")
-     + scale_y_continuous(labels = "F0")
-	 + theme_default()
-	 ;
+    bitmap(file = file, size = [2700, 1800]) {
+        ggplot(data, aes(x = "errors"), padding = "padding:250px 400px 200px 250px;")
+        + geom_histogram(bins = 250,  color = "steelblue", range = [0, 1])
+        + ggtitle(title)
+        + scale_x_continuous(labels = "F2")
+        + scale_y_continuous(labels = "F0")
+        + theme_default()
+        ;
+    }
 }
+
+# test errors of SVR
+plot_errors(
+    read.csv("./svr_test.csv", row.names = 1), 
+    "Frequency of SVR Prediction Errors", 
+    "./svr_test.png"
+);
+
+# test errors of xgboost regression
+plot_errors(
+    read.csv("./regression_test.csv", row.names = 1), 
+    "Frequency of xgboost regression errors", 
+    "./xgboost_regression_test.png"
+);
