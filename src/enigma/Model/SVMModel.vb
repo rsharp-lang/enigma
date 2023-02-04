b70c43afa311d4adf5bfbf00721d99b893f42a68
xieguigang <xie.guigang@gcmodeller.org>
2023-02-05 01:20:25 +0800

    create svr data model

--- src/enigma/Model/SVMModel.vb
+++ src/enigma/Model/SVMModel.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.DataMining.ComponentModel.Encoder
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.MachineLearning
 Imports Microsoft.VisualBasic.MachineLearning.SVM
 Imports SMRUCC.Rsharp.Runtime
@@ -11,12 +12,17 @@ Public Class SVMModel : Inherits MLModel
     Public Property svr As Boolean = False
 
     Public Overrides Function DoCallTraining(args As list, env As Environment) As MLModel
-        Dim labels As String() = REnv.asVector(Of String)(data(Me.Labels(Scan0)))
-        Dim problem = svmDataSet.svmProblem(Features, labels, data, env)
+        Dim problem As [Variant](Of Message, SVM.Problem)
         Dim params As New SVM.Parameter With {
             .svmType = If(svr, SvmType.EPSILON_SVR, SvmType.C_SVC)
         }
         Dim weights As Dictionary(Of String, Double) = args.getValue("weights", env, New Dictionary(Of String, Double))
+
+        If svr Then
+            problem = svmDataSet.svrProblem(Features, Labels(Scan0), data, env)
+        Else
+            Problem = svmDataSet.svmProblem(Features, REnv.asVector(Of String)(data(Me.Labels(Scan0))), data, env)
+        End If
 
         If problem Like GetType(Message) Then
             Return New MLPipelineError(problem.TryCast(Of Message))
