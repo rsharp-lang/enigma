da39ece7c4948b158df960e10f2df1b60ab5f595
guigang xie <xie.guigang@gcmodeller.org>
2023-02-06 15:28:09 +0800

    run data test

--- test/randomForest_regression.R
+++ test/randomForest_regression.R
@@ -6,15 +6,18 @@ setwd(@dir);
 
 let x = 1:1000;
 let y = x ^ 1.25 + runif(n = length(x));
-let z = y / x;
+let a = (x ^ 2) * runif(n = length(x));
+let b = a / 2 + y;
+let c = 1 + runif(n = length(x));
+let z = y / x + b;
 
-data = data.frame(x, y, z, row.names = as.character(x));
+data = data.frame(x, y, a, b, c, z, row.names = as.character(x));
 
 print(data, max.print = 6);
 cat("\n\n\n");
 
 test = tensor(model = model::randomForest(regression = TRUE))
-|> feed(data, features = ["x", "y"])
+|> feed(data, features = ["x", "y","a","b","c"])
 |> output(labels = "z")
 |> learn()
 |> solve(data)
