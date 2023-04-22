19883490ec165f0d05de953190a355b676a7e625
xieguigang <xie.guigang@gcmodeller.org>
2023-04-22 21:02:13 +0800

    split source files

--- src/CUDA/CudaSharp.vb
+++ src/CUDA/CudaSharp.vb
@@ -8,19 +8,6 @@ Imports System.Text
 Imports LLVM
 
 Namespace CudaSharp
-    ''' <summary>
-    ''' Generic exception from CudaSharp
-    ''' </summary>
-    Public Class CudaSharpException
-        Inherits Exception
-        ''' <summary>
-        ''' Creates a CudaSharp exception
-        ''' </summary>
-        ''' <param name="message">Message</param>
-        Public Sub New(ByVal message As String)
-            MyBase.New(message)
-        End Sub
-    End Class
 
     ''' <summary>
     ''' Main CudaSharp class
---
+++ src/CUDA/CudaSharpException.vb
@@ -0,0 +1,13 @@
+﻿''' <summary>
+''' Generic exception from CudaSharp
+''' </summary>
+Public Class CudaSharpException
+    Inherits Exception
+    ''' <summary>
+    ''' Creates a CudaSharp exception
+    ''' </summary>
+    ''' <param name="message">Message</param>
+    Public Sub New(ByVal message As String)
+        MyBase.New(message)
+    End Sub
+End Class
