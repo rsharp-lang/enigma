f154937768fbff781afba25a3bfc23510bd458fe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 16:58:52 +0800

    update ts/js type export header files

--- @export/learning.d.ts
+++ @export/learning.d.ts
@@ -1,5 +1,7 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
+//    imports "learning" from "enigma";
+//
 // ref=enigma.learning@enigma, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
--- @export/model.d.ts
+++ @export/model.d.ts
@@ -1,5 +1,7 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
+//    imports "model" from "enigma";
+//
 // ref=enigma.models@enigma, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
