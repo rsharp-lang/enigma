5eed2a10b6053fdbf6a4568b4391b823985e44f0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-29 20:32:36 +0800

    build documents

---
+++ @export/gpu.d.ts
@@ -0,0 +1,21 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "gpu" from "enigma";
+//
+// ref=enigma.GPU@enigma, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace gpu {
+   /**
+    * try to execute the expression on gpu
+    * 
+    * 
+     * @param exp -
+     * @param env -
+     * 
+     * + default value Is ``null``.
+   */
+   function exec(exp: object, env?: object): any;
+}
---
+++ man/enigma/gpu/gpu.md
@@ -0,0 +1,5 @@
+# gpu
+
+
+
++ [exec](gpu/exec.1) try to execute the expression on gpu
