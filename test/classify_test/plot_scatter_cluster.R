1139a6115326e57e3cd82003f5b47ca1a2916031
xieguigang <gg.xie@bionovogene.com>
2023-06-08 14:02:46 +0800

    sgt is not working with UMAP

--- test/classify_test/plot_scatter_cluster.R
+++ test/classify_test/plot_scatter_cluster.R
@@ -2,11 +2,11 @@ require(ggplot);
 
 setwd(@dir);
 
-let plot_data = function(data, savefile) {
+let plot_data = function(data, savefile, mapping = aes(x="x",y="y", z="z")) {
     bitmap(file = savefile, size = [2400, 1800]) {
 
 
-        ggplot(data, aes(x="x",y="y", z="z"), padding = [150, 650, 100, 100])
+        ggplot(data, mapping, padding = [150, 650, 100, 100])
         + geom_point(aes(color = "class"), 
         color = "paper", shape = "circle", size = 10, show.legend = FALSE)
         + view_camera(angle = [31.5,65,125], fov = 100000)
@@ -17,5 +17,5 @@ let plot_data = function(data, savefile) {
 }
 
 plot_data(read.csv("./protein_umap.csv", row.names = 1, check.names = FALSE), savefile = "./protein_umap.png");
-plot_data(read.csv("./protein_pca.csv", row.names = 1, check.names = FALSE), savefile = "./protein_pca.png");
+plot_data(read.csv("./protein_pca.csv", row.names = 1, check.names = FALSE), savefile = "./protein_pca.png", mapping = aes(x="dim1",y="dim2", z="dim3"));
 
