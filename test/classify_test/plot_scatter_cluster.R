8ff4385acbbba1b5c406dbce26df99a601ee3b4f
xieguigang <gg.xie@bionovogene.com>
2023-06-08 14:01:07 +0800

    plot data

--- test/classify_test/plot_scatter_cluster.R
+++ test/classify_test/plot_scatter_cluster.R
@@ -2,16 +2,20 @@ require(ggplot);
 
 setwd(@dir);
 
-let data = read.csv("./protein_umap.csv", row.names = 1, check.names = FALSE);
+let plot_data = function(data, savefile) {
+    bitmap(file = savefile, size = [2400, 1800]) {
 
-bitmap(file = "./protein_umap.png", size = [2400, 1800]) {
 
-
-    ggplot(data, aes(x="x",y="y", z="z"), padding = [150, 650, 100, 100])
-    + geom_point(aes(color = "class"), 
-    color = "paper", shape = "circle", size = 10, show.legend = FALSE)
-	+ view_camera(angle = [31.5,65,125], fov = 100000)
-	+ ggtitle("Scatter UMAP 3D")
-	# + theme_default()
-	;
+        ggplot(data, aes(x="x",y="y", z="z"), padding = [150, 650, 100, 100])
+        + geom_point(aes(color = "class"), 
+        color = "paper", shape = "circle", size = 10, show.legend = FALSE)
+        + view_camera(angle = [31.5,65,125], fov = 100000)
+        + ggtitle("Scatter UMAP 3D")
+        # + theme_default()
+        ;
+    }
 }
+
+plot_data(read.csv("./protein_umap.csv", row.names = 1, check.names = FALSE), savefile = "./protein_umap.png");
+plot_data(read.csv("./protein_pca.csv", row.names = 1, check.names = FALSE), savefile = "./protein_pca.png");
+
