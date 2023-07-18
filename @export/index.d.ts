f637f00d3195f96753a48205e733f911e734e189
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-18 18:47:57 +0800

    data_aishell3 helper

--- @export/index.d.ts
+++ @export/index.d.ts
@@ -6,11 +6,17 @@ declare namespace enigma {
    module _ {
       /**
       */
+      function aishell3_pinyin(pinyin_lib: any, si: any): object;
+      /**
+      */
       function onLoad(): object;
       /**
       */
       function pinyin(pinyin_lib: any, si: any): object;
    }
+   /**
+   */
+   function data_aishell3(text: any): object;
    /**
    */
    function transcript_pinyin(text: any): object;
--- R/pinyin.R
+++ R/pinyin.R
@@ -12,6 +12,29 @@ const transcript_pinyin = function(text) {
     }
 }
 
+const data_aishell3 = function(text) {
+    const pinyin_lib = clr::open("Microsoft.VisualBasic.Text.GB2312");
+
+    if (length(text) == 1) {
+        .aishell3_pinyin(pinyin_lib, text);
+    } else {
+        lapply(text, si -> .aishell3_pinyin(pinyin_lib, si));
+    }
+}
+
+const .aishell3_pinyin = function(pinyin_lib, si) {
+    let pinyin = clr::call_clr(pinyin_lib, "TranscriptPinYin", str = si, sep = " | ");
+    let split2 = clr::call_clr(pinyin_lib, "SplitZhChars", str = si, sep = " | ");
+    
+    split2 = split2[nchar(split2) > 0];
+    pinyin = pinyin 
+    |> strsplit(" | ", fixed = TRUE) 
+    |> tolower()
+    ;
+
+    data.frame(pinyin, chs = split2);
+}
+
 const .pinyin = function(pinyin_lib, si) {
     let pinyin = clr::call_clr(pinyin_lib, "TranscriptPinYin", str = si, sep = " | ");
     
