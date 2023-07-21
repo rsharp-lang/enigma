ec499691ab878de045296364bd7557ba47b64d68
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-22 02:45:51 +0800

    Update aishell3.R

--- tools/aishell3.R
+++ tools/aishell3.R
@@ -2,13 +2,19 @@ require(enigma);
 
 const dir as string = ?"--dir" || stop("no inputs!");
 const text as string = list.files(dir, pattern = "*.txt");
-const data as string = sapply(text, function(file) {
+const data = lapply(text, function(file) {
     const wav = `${basename(file)}.wav`;
     const text = enigma::data_aishell3(readText(file));
+    const content = `${wav} ${text$data_aishell3}`;
+    const label = `${basename(file)}|${paste(text$chars$pinyin, " ")} $|${text$text}$`;
 
-    `${wav} ${text$data_aishell3}`;
+    {
+        content: content,
+        label: label
+    }
 });
 
-print(data);
+str(data);
 
-writeLines(data, con = `${dir}/content.txt`);
+writeLines(data@content, con = `${dir}/content.txt`);
+writeLines(data@label, con = `${dir}/label_train-set.txt`);
