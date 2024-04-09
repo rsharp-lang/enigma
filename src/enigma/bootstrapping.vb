79aee2fd3a48e6615687f5141bcd407582a387f2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 09:39:40 +0800

    call algorithm by name

--- src/enigma/bootstrapping.vb
+++ src/enigma/bootstrapping.vb
@@ -1,16 +1,19 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MachineLearning.Bootstrapping
 Imports Microsoft.VisualBasic.MachineLearning.Bootstrapping.GraphEmbedding
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMRUCC.Rsharp.Runtime.Vectorization
 
 <Package("bootstrapping")>
 Module bootstrapping
 
     <ExportAPI("arguments")>
     Public Function arguments() As Arguments
-
+        Return New Arguments
     End Function
 
     <ExportAPI("complex")>
@@ -20,5 +23,17 @@ Module bootstrapping
                             Optional rules As String = Nothing,
                             Optional env As Environment = Nothing) As Object
 
+        Dim alg As String = CLRVector.asCharacter(algorithm).DefaultFirst("ComplEx")
+
+        args.other = New Dictionary(Of String, String) From {{"rules", rules}}
+
+        Select Case alg.ToLower
+            Case "complex" : Return GraphEmbedding.Algorithm.learn(Of GraphEmbedding.complex.ComplEx)(args)
+            Case "complex_nne" : Return GraphEmbedding.Algorithm.learn(Of GraphEmbedding.complex_NNE.ComplEx)(args)
+            Case "complex_nne_aer" : Return GraphEmbedding.Algorithm.learn(Of GraphEmbedding.complex_NNE_AER.ComplEx)(args)
+            Case "complex_r" : Return GraphEmbedding.Algorithm.learn(Of GraphEmbedding.complex_R.ComplEx)(args)
+            Case Else
+                Return Internal.debug.stop($"invalid algorithm name({alg})!", env)
+        End Select
     End Function
 End Module
--- test/graph_embedding_datasets/test.R
+++ test/graph_embedding_datasets/test.R
@@ -1,3 +1,8 @@
+require(enigma);
+
+imports "bootstrapping" from "enigma";
+
 # Datasets we used are in the corresponding subfolder contained in datasets/ with the following formats:
 # * _train.txt,_valid.txt,_test.txt; training, valid, test set with string id; format: **e1**\t**r**\t**e2**\n
 # * _cons.txt; approximate entailment constraints; formant: **r1,r2**\t**confidence**\n, where '-' denotes the inversion
+
