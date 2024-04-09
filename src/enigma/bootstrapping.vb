608829d616cb376bedca07e6540b9793d0d950fe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 13:34:32 +0800

    make improvements of the graph builder model

--- src/enigma/bootstrapping.vb
+++ src/enigma/bootstrapping.vb
@@ -6,6 +6,7 @@ Imports Microsoft.VisualBasic.MachineLearning.Bootstrapping.node2vec
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.Rsharp.Runtime.Vectorization
 
@@ -50,13 +51,50 @@ Module bootstrapping
         Dim g As node2vec.Graph
 
         If TypeOf graph Is String Then
-            g = New node2vec.Graph(graph, False)
+            g = loadGraphFrom(file:=graph)
         ElseIf TypeOf graph Is node2vec.Graph Then
             g = graph
+        ElseIf TypeOf graph Is dataframe Then
+            Dim df = DirectCast(graph, dataframe)
+            Dim u As String() = CLRVector.asCharacter(df.getBySynonym("source", "u"))
+            Dim v As String() = CLRVector.asCharacter(df.getBySynonym("target", "v"))
+            Dim w As Double() = CLRVector.asNumeric(df.getBySynonym("weight", "w"))
+
+            g = Solver.CreateGraph(u, v, w)
         Else
             Throw New NotImplementedException
         End If
 
         Return Solver.CreateEmbedding(g, dimensions:=dims)
+    End Function
+
+    ''' <summary>
+    ''' load graph data from file
+    ''' input format: node1_id_int node2_id_int &lt;weight_float> </summary>
+    ''' <param name="file"> path of the input file </param>
+    Private Function loadGraphFrom(file As String) As Graph
+        Dim u As New List(Of String)
+        Dim v As New List(Of String)
+        Dim w As New List(Of Double)
+
+        ' read graph info from file
+        For Each lineTxt As String In file.IterateAllLines
+            ' parse the line text to get the edge info
+            Dim strList = lineTxt.Split(" "c)
+            Dim node1ID = strList(0)
+            Dim node2ID = strList(1)
+
+            Call u.Add(node1ID)
+            Call v.Add(node2ID)
+
+            ' add the edge to the graph
+            If strList.Length > 2 Then
+                Call w.Add(Double.Parse(strList(2)))
+            Else
+                Call w.Add(1.0)
+            End If
+        Next
+
+        Return Solver.CreateGraph(u.ToArray, v.ToArray, w.ToArray)
     End Function
 End Module
