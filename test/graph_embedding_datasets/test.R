11c51c4221f2de82ccb38ad6b58f16c92ff0bcb1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 09:47:40 +0800

    configs run debug

--- test/graph_embedding_datasets/test.R
+++ test/graph_embedding_datasets/test.R
@@ -6,3 +6,13 @@ imports "bootstrapping" from "enigma";
 # * _train.txt,_valid.txt,_test.txt; training, valid, test set with string id; format: **e1**\t**r**\t**e2**\n
 # * _cons.txt; approximate entailment constraints; formant: **r1,r2**\t**confidence**\n, where '-' denotes the inversion
 
+setwd(@dir);
+
+let args = bootstrapping::arguments(
+    fnTrainTriples = "DB100K\\_train.txt", 
+    fnValidTriples = "DB100K\\_valid.txt", 
+    fnTestTriples = "DB100K\\_test.txt", 
+    fnAllTriples = NULL
+);
+
+bootstrapping::complex(args);
