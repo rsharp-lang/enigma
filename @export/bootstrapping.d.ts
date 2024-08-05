83839ea74e4ef6f7e35c6844fd8d1456741735fa
谢桂纲 <gg.xie@bionovogene.com>
2024-08-05 11:12:38 +0800

    config project profile for run msbuild

--- @export/bootstrapping.d.ts
+++ @export/bootstrapping.d.ts
@@ -5,15 +5,24 @@
 // ref=enigma.bootstrapping@enigma, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * 
 */
 declare namespace bootstrapping {
    /**
    */
-   function arguments(): object;
+   function arguments(fnTrainTriples: string, fnValidTriples: string, fnTestTriples: string, fnAllTriples: string): object;
    /**
      * @param algorithm default value Is ``["ComplEx","complex_NNE","complex_NNE_AER","complex_R"]``.
      * @param rules default value Is ``null``.
+     * @param iterations default value Is ``1000``.
      * @param env default value Is ``null``.
    */
-   function complex(args: object, algorithm?: any, rules?: string, env?: object): any;
+   function complex(args: object, algorithm?: any, rules?: string, iterations?: object, env?: object): any;
+   /**
+   */
+   function graph2vec(graph: any): any;
+   /**
+     * @param dims default value Is ``10``.
+   */
+   function node2vec(graph: any, dims?: object): any;
 }
--- man/enigma/bootstrapping/bootstrapping.md
+++ man/enigma/bootstrapping/bootstrapping.md
@@ -4,3 +4,5 @@
 
 + [arguments](bootstrapping/arguments.1) 
 + [complex](bootstrapping/complex.1) 
++ [graph2vec](bootstrapping/graph2vec.1) 
++ [node2vec](bootstrapping/node2vec.1) 
